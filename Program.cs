36fa1df3de846ef02edafb1b182c24fc34bbbad1
Igor <igor@sysdesign.pl>
2025-01-10 21:44:56 +0100

    Code cleaning

--- Program.cs
+++ Program.cs
@@ -13,33 +13,25 @@ namespace payment_service
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
             builder.Services.AddDbContext<PaymentDatabaseContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
             builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
             builder.Services.AddScoped<IPaymentService, PaymentService>();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
 
-			/**
-             * Authorization with gateway api by ip filtering
-             */
+
 			app.UseMiddleware<IpFilteringMiddleware>();
 
-			/**
-             * Authorization with gateway api by secret key
-             */
 			app.UseMiddleware<GatewayAuthenticationMiddleware>();
 
 			app.UseHttpsRedirection();
